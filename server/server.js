a3f5bb918422660fe4873c0a105f9a0a94da44f8
raiyantrimax <raiyan.trimax@gmail.com>
2025-12-24 00:17:42 +0530

    fix: Update MongoDB connection options to remove deprecated parameters

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(uploadDir));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
